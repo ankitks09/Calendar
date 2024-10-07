3c387051c0d32631877a7ac8e59de6a58f3e95f9
Ankit Singh <163592737+ankitks09@users.noreply.github.com>
2024-10-07 23:39:10 +0530

    Update calendar_c++.cpp

--- calendar_c++.cpp
+++ calendar_c++.cpp
@@ -1,18 +1,18 @@
 #include <iostream>
 #include <conio.h>
 #include <windows.h>
-
 using namespace std;
 
-int getkey();
-void display(int, int, int, int[]);
-void calendar(int, int);
+const char* month[] = {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
+const char* week[] = {"Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"};
 
-void gotoxy(int x, int y) {
-    COORD coord;
-    coord.X = x;
-    coord.Y = y;
-    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
+void gotoxy(int x, int y) 
+{
+    COORD value;
+    value.X = x;
+    value.Y = y;
+
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), value);
 }
 
 void SetColor(int ForgC) {
@@ -27,75 +27,17 @@ void SetColor(int ForgC) {
     return;
 }
 
-const char* month[] = {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
-const char* week[] = {"Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"};
-
-int main() {
-    int nmonth, nyr, ch;
-enteryear:
-    while (1) {
-        cout << "Enter year and month(number): ";
-        cin >> nyr >> nmonth;
-        if (nyr < 1945) {
-            cout << "\n\tPlease enter year above 1945\n";
-            continue;
-        } else {
-            calendar(nyr, nmonth);
-        }
-        while (1) {
-            gotoxy(20, 20); cout << "(*) Use LEFT, RIGHT, UP and DOWN arrow.";
-            gotoxy(20, 22); cout << "(*) Press P to go to particular year and month.";
-            gotoxy(20, 24); cout << "(*) Press ESC to Exit.";
-            ch = getkey();
-            switch (ch) {
-                case 80: // DOWN ARROW
-                    if (nmonth == 12) {
-                        nmonth = 1;
-                        nyr++;
-                    } else {
-                        nmonth++;
-                    }
-                    calendar(nyr, nmonth);
-                    break;
-                case 77: // RIGHT ARROW
-                    nyr++;
-                    calendar(nyr, nmonth);
-                    break;
-                case 72: // UP ARROW
-                    if (nmonth == 1) {
-                        nyr--;
-                        nmonth = 12;
-                    } else
-                        nmonth--;
-                    calendar(nyr, nmonth);
-                    break;
-                case 75: // LEFT ARROW
-                    if (nyr == 1945) {
-                        gotoxy(15, 2); cout << "Year below 1945 not available";
-                    } else {
-                        nyr--;
-                        calendar(nyr, nmonth);
-                    }
-                    break;
-                case 27: // ESC KEY
-                    system("cls");
-                    gotoxy(50, 14); cout << "Exiting program.\n\n\n\n\n";
-                    exit(0);
-                case 112: // P KEY
-                    system("cls");
-                    goto enteryear;
-            }
-        }
-        break;
-    }
-    getch();
-    return 0;
+int getkey() 
+{
+    return getch();
 }
+
 
 void display(int nyr, int nmonth, int tdays, int days[]) {
     int i, j, pos;
     SetColor(12);
     gotoxy(56, 6); cout << month[nmonth - 1] << " " << nyr;
+
     for (i = 0, pos = 30; i < 7; i++, pos += 10) {
         if (i == 6)
             SetColor(9);
@@ -106,8 +48,8 @@ void display(int nyr, int nmonth, int tdays, int days[]) {
 
     SetColor(15);
 
-    tdays++;
-    if (tdays == 0 || tdays == 7)
+    
+    if (tdays == 7 )//|| tdays == 0)
         pos = 91;
     if (tdays == 1)
         pos = 31;
@@ -122,12 +64,13 @@ void display(int nyr, int nmonth, int tdays, int days[]) {
     if (tdays == 6)
         pos = 81;
 
-    for (i = 0, j = 10, pos; i < days[nmonth - 1]; i++, pos += 10) {
+    for (i = 1, j = 10, pos; i < days[nmonth - 1]; i++, pos = pos+10) 
+    {
         if (pos == 91)
             SetColor(8);
         else
             SetColor(7);
-        gotoxy(pos, j); cout << i + 1;
+        gotoxy(pos, j); cout << i;
         if (pos == 91) {
             pos = 21;
             j++;
@@ -157,40 +100,114 @@ void display(int nyr, int nmonth, int tdays, int days[]) {
     gotoxy(98, 11); cout << char(175);
 }
 
-int getkey() {
-    int ch;
-    ch = getch();
-    if (ch == 0) {
-        cout << "zero";
-        ch = getch();
-        return ch;
-    }
-    return ch;
-}
-
-void calendar(int nyr, int nmonth) {
+void calendar(int curryear, int nmonth) 
+{
     int days[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-    int tdays = 0, k, myear;
+    int tdays = 0, k, prevyear;
     system("cls");
-    myear = nyr - 1;
-    if (myear >= 1945) {
-        for (k = 1945; k <= myear; k++) {
-            if (k % 4 == 0)
+
+    prevyear = curryear - 1;
+
+    if (prevyear >= 1945) 
+    {
+        for (k = 1945; k <= prevyear; k++) {
+            if ((k % 4 == 0 && k%100!=0) || (k%100==0 && k%400==0)) //leap year
                 tdays = tdays + 366;
             else
                 tdays = tdays + 365;
         }
     }
 
-    if (nyr % 4 == 0)
+    if ((k % 4 == 0 && k%100!=0) || (k%100==0 && k%400==0))
         days[1] = 29;
-    else
-        days[1] = 28;
+    
 
     for (k = 0; k < (nmonth - 1); k++) {
         tdays = tdays + days[k];
     }
 
     tdays = tdays % 7;
-    display(nyr, nmonth, tdays, days);
+    tdays++;
+    display(curryear, nmonth, tdays, days);
+}
+
+int main() 
+{
+    int nmonth, nyr;
+enteryear:
+    while (1) 
+    {
+        cout << "Enter year and month(number): ";
+        cin >> nyr >> nmonth;
+
+        if (nyr < 1945) {
+            cout << "\n\tPlease enter year above 1945\n";
+            continue;
+        } else {
+            calendar(nyr, nmonth);
+        }
+        //bhbjjbbj
+        while (1) {
+            gotoxy(20, 20); cout << "(*) Use LEFT, RIGHT, UP and DOWN arrow.";
+            gotoxy(20, 22); cout << "(*) Press P to go to particular year and month.";
+            gotoxy(20, 24); cout << "(*) Press ESC to Exit.";
+
+            int ch = getkey();
+
+            if (ch == 80) 
+            { // DOWN ARROW
+                if (nmonth == 12) 
+                {
+                    nmonth = 1;
+                    nyr++;
+                }
+                else 
+                {
+                    nmonth++;
+                }
+                calendar(nyr, nmonth);
+            } 
+            else if (ch == 77) { // RIGHT ARROW
+                nyr++;
+                calendar(nyr, nmonth);
+            }
+
+            else if (ch == 72) { // UP ARROW
+                if (nmonth == 1) {
+                    nyr--;
+                    nmonth = 12;
+                } else 
+                {
+                    nmonth--;
+                }
+                calendar(nyr, nmonth);
+            } 
+            
+            else if (ch == 75) { // LEFT ARROW
+                if (nyr == 1945) {
+                    gotoxy(15, 2); cout << "Year below 1945 not available";
+                } else {
+                    nyr--;
+                    calendar(nyr, nmonth);
+                }
+            } 
+
+            else if (ch == 27) { // ESC KEY
+                system("cls");
+                gotoxy(50, 14); cout << "Exiting program.\n\n\n\n\n";
+                //exit(0);
+
+                return 0;
+            } 
+            
+            else if (ch == 112) { // P KEY
+                system("cls");
+                goto enteryear;
+            }
+
+        }
+        break;
+    }
+    getch();
+    return 0;
 }
